b0c5df1bd433d5336627e159c5484132fd2d1c2c
hanleeso <hanleeso1@korea.ac.kr>
2023-10-08 20:29:24 +0900

    Change Date : 2023. 10. 08 sort 함수 추가

--- src/util.py
+++ src/util.py
@@ -1,12 +1,13 @@
 #[TITLE] Utility
 #[DESC] CyberHawk에서 사용하는 기능들 구현
-#       현재 기능: ls, mkdir, rm, touch, cp, mv, file_sig
+#       현재 기능: ls, mkdir, rm, touch, cp, mv, file_sig, sort
 #[Writer] 
 
 import enum
 import os
 import shutil
 import binwalk
+import Sort # sort 함수 동작을 위한 자체 제작 라이브러리
 
 # 파일 시그니처와 파일 확장자를 쌍으로 미리 저장
 # 텍스트 파일의 형식은 별도의 처리를 위해 따로 저장
@@ -62,11 +63,11 @@ def ls():
     files = os.listdir(current_directory)
     print("현재 디렉토리 내의 파일 목록:")
     for file in files:
-    if os.path.isdir(file):
-        print(file)
-    elif os.path.isfile(file):
-        fileExt = os.path.splitext(file)[1]
-        checkFileSignature(fileExt, file)
+        if os.path.isdir(file):
+            print(file)
+        elif os.path.isfile(file):
+            fileExt = os.path.splitext(file)[1]
+            checkFileSignature(fileExt, file)
 
 def mkdir(args):
     try:
@@ -123,3 +124,67 @@ def file_sig(args): #파일 시그니쳐 출력 함수   ex) file_sig("filename"
     except Exception as e:
         print(f"binwalk 오류 발생: {e}")
 
+'''
+* Name : sort
+
+* Description : format, size, time, name 4가지 기능별 정렬 함수
+    - format : 포맷 타입을 입력 받은 후 현재 디렉토리에서 해당 포맷 타입을 가진 파일을 출력
+    - size : 현재 디렉토리에 있는 파일들 중 '파일 크기가 가장 작은 것' 부터 순차적으로 출력 (디렉토리는 출력 안함)
+    - time : 가장 최근에 변경한 적이 있는 파일부터 순차적으로 출력하며 연도-월-일-시-분 도 같이 출력
+    - name : 이름 순으로 정렬('영어 기준' 영어가 먼저 출력된 후 한글 파일들이 나오며 한글 자음 기준 정렬은 아직 구현 x)
+
+* Arguments :   -args : 문자열 값으로 'format', 'size', 'time', 'name' 중 하나가 입력됨
+
+* Change Date : 2023. 10. 08
+
+* Version : 0.2                
+
+'''
+def sort(args): 
+    mode = ['format', 'size', 'time', 'name']
+    current_directory = os.getcwd()
+    files = os.listdir(current_directory)
+    #try:
+    if (args == mode[0]):
+        uformat = input('input format type : ')
+        format_dict = {}
+
+        for file in files:
+            format = file.split('.')[-1]
+            if format not in format_dict:
+                format_dict[format] = [file]
+            else:
+                format_dict[format].append(file)
+
+        if (uformat in list(format_dict.keys())):        
+            f = format_dict.get(uformat, [])
+            print('\n')
+            for file in f:
+                print(file)
+        else :
+            print(f'Can not find format {uformat}')
+
+    elif (args == mode[1]):   # size sort (파일 크기 기준 정렬)      
+        current_dir = os.getcwd() 
+        files_in_current_dir = Sort.size_sort().size_list_files_in_current_dir(current_dir)
+        sorted_files = Sort.size_sort().heap_sort_by_size(files_in_current_dir)
+        for file_name, file_size in sorted_files:
+            print(file_name, end = '  ')
+            Sort.size_sort().get_size(file_size)
+
+    elif (args == mode[2]): # time sort (최종 변경 시간 기준 정렬)
+        current_dir = os.getcwd()
+        files_in_current_dir = Sort.time_sort().time_list_files_in_current_dir(current_dir)
+        sorted_files = Sort.time_sort().heap_sort_by_created_time(files_in_current_dir)
+        for file_name, file_modified_time in sorted_files:
+            print('%-50s%20s' %(str(file_name), str(file_modified_time)))
+
+    elif (args == mode[3]): # name sort(영어 기준)
+        current_directory = os.getcwd()
+        sorted_file_list = Sort.name_sort().sort_files_by_name(current_directory)
+        for file_name in sorted_file_list:
+            print(file_name)                        
+    else :
+        print('잘못된 명령어입니다.')
+    #except :
+        #print('에러 발생.')
